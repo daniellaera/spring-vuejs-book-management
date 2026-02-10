781cac00126c9c6ff4a4947ea8c8c82ea0d207c7
daniellaera <admin@example.com>
2026-02-10 10:09:49 +0100

    more test & frontend

--- backend/src/main/java/com/daniellaera/backend/utils/NameGenerator.java
+++ backend/src/main/java/com/daniellaera/backend/utils/NameGenerator.java
@@ -10,7 +10,18 @@ public class NameGenerator {
             "Graceful", "Heroic", "Infinite", "Invincible", "Loyal", "Majestic",
             "Mighty", "Noble", "Radiant", "Resilient", "Sharp", "Silent", "Swift",
             "Tenacious", "Valiant", "Vibrant", "Wise", "Witty", "Zealous", "Ethereal",
-            "Fearless", "Mystic", "Playful", "Proud", "Serene", "Spirited", "Vivid"
+            "Fearless", "Mystic", "Playful", "Proud", "Serene", "Spirited", "Vivid",
+            "Amber", "Arctic", "Astral", "Blazing", "Breezy", "Celestial", "Crimson",
+            "Crystal", "Cunning", "Defiant", "Deft", "Devoted", "Distant", "Dreamy",
+            "Dusky", "Electric", "Emerald", "Enchanted", "Eternal", "Feral", "Fiery",
+            "Frosty", "Gentle", "Gilded", "Grim", "Hallowed", "Humble", "Icy",
+            "Jade", "Keen", "Kindred", "Lavish", "Lofty", "Lucid", "Lunar",
+            "Marine", "Mellow", "Molten", "Nimble", "Obsidian", "Onyx", "Opulent",
+            "Pacific", "Phantom", "Primal", "Pristine", "Quick", "Quiet", "Regal",
+            "Roaming", "Rugged", "Rustic", "Sacred", "Savage", "Scarlet", "Shadow",
+            "Silver", "Solar", "Somber", "Stark", "Stellar", "Stoic", "Stormy",
+            "Stout", "Subtle", "Sunlit", "Tidal", "Timeless", "Twilight", "Untamed",
+            "Verdant", "Volcanic", "Wandering", "Wicked", "Wild", "Wintry", "Zen"
     };
 
     private static final String[] NOUNS = {
@@ -20,7 +31,18 @@ public class NameGenerator {
             "Cobra", "Bison", "Crane", "Stallion", "Pegasus", "Orca", "Husky",
             "Owl", "Puma", "Serpent", "Otter", "Dolphin", "Penguin", "Ram",
             "Mustang", "Tortoise", "Crocodile", "Koala", "Panda", "Elephant",
-            "Hedgehog", "Antelope", "Foxhound", "Sphinx", "Unicorn", "Mongoose"
+            "Hedgehog", "Antelope", "Foxhound", "Sphinx", "Unicorn", "Mongoose",
+            "Armadillo", "Badger", "Barracuda", "Basilisk", "Bobcat", "Buffalo",
+            "Caribou", "Chameleon", "Cicada", "Condor", "Coyote", "Dingo",
+            "Dragonfly", "Elk", "Firefly", "Gazelle", "Gecko", "Gorilla",
+            "Grizzly", "Heron", "Hornet", "Hyena", "Iguana", "Impala",
+            "Jackal", "Jellyfish", "Kestrel", "Kingfisher", "Kraken", "Lemur",
+            "Mammoth", "Mantis", "Marlin", "Marten", "Merlin", "Mockingbird",
+            "Moose", "Narwhal", "Nighthawk", "Ocelot", "Osprey", "Oyster",
+            "Parrot", "Pelican", "Python", "Quail", "Raptor", "Rhino",
+            "Salamander", "Scorpion", "Sparrow", "Stingray", "Swallow", "Swordfish",
+            "Talon", "Tapir", "Terrier", "Thunderbird", "Toucan", "Trout",
+            "Vulture", "Walrus", "Warthog", "Wasp", "Wolverine", "Wren", "Yak"
     };
 
     private static final Random RANDOM = new Random();
